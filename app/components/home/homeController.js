c1cfa1df9269ba56893317989aeb30fa8219c64d
David Castillo <davecas26@gmail.com>
2015-04-05 17:38:52 -0430

    se creo que objeto pokemons y se agregoa la vista

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -4,7 +4,35 @@
     .controller('homeController', [ 
     '$scope', 
     function ($scope) {
-        console.log('cargamos');
+        $scope.pokemons = {
+		    "id": "001",
+		    "name": "Bulbasaur",
+		    "species": "Seed Pokémon",
+		    "type": [
+		      "Grass",
+		      "Poison"
+		    ],
+		    "height": "2′4″ (0.71m)",
+		    "weight": "15.2 lbs (6.9 kg)",
+		    "abilities": [
+		      "Overgrow",
+		      "Chlorophyll"
+		    ],
+		    "stats": {
+		      "hp": 45,
+		      "attack": 49,
+		      "defense": 49,
+		      "sp.atk": 65,
+		      "sp.def": 65,
+		      "speed": 45,
+		      "total": 318
+		    },
+		    "evolution": [
+		      "Bulbasaur",
+		      "Ivysaur",
+		      "Venusaur"
+		    ]
+  		};
     }]);
 
 })();
