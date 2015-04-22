6736f365b883d4bf1eb6d4e468398704675ede44
David Castillo <davecas26@gmail.com>
2015-04-21 21:58:11 -0430

    se agrego el servicio al proyecto

---
+++ app/components/pokemons/pokemonsService.js
@@ -0,0 +1,11 @@
+angular.module('pokemonsService', [])
+
+   .factory('pokemonsFactory', ['$http', '$q', 'api',
+   function ($http, $q, api){
+      
+      
+
+      return {
+        
+      };
+}]);
