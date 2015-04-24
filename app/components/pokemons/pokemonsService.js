e0525d3e2d796dda76280df1a98753d5664477b7
David Castillo <davecas26@gmail.com>
2015-04-24 02:16:09 -0430

    se creo una promesa para obtener todos los datos de lso pokemonos

--- app/components/pokemons/pokemonsService.js
+++ app/components/pokemons/pokemonsService.js
@@ -3,9 +3,20 @@ angular.module('pokemonsService', [])
    .factory('pokemonsFactory', ['$http', '$q', 'api',
    function ($http, $q, api){
       
-      
+      	function all(){
+      		var deferred = $q.defer();
 
-      return {
-        
-      };
+      		$http.get('assets/json/pokemons.json')
+            .success(function (data) {
+                deferred.resolve(data);
+            })
+            .error(function(data) {
+            });
+
+            return deferred.promise;
+      	}
+
+		return {
+			all: all
+		};
 }]);
