36e8e7d4c010fb248535701cf957455479c689e4
David Castillo <davecas26@gmail.com>
2015-05-02 18:52:23 -0430

    se agrego el filtrado de tipos

--- app/components/pokemons/pokemonsController.js
+++ app/components/pokemons/pokemonsController.js
@@ -2,14 +2,28 @@
     angular.module('pokemonsController', ['pokemonsService','pokemonNameDirective','pokemonPictureDirective','pokemonTypeDirective'])
 
     .controller('pokemonsController', [ 
-    '$scope', '$http', 'pokemonsFactory',
-    function ($scope, $http, pokemonsFactory) {
+    '$scope', '$http', 'pokemonsFactory', '$routeParams',
+    function ($scope, $http, pokemonsFactory, $routeParams) {
     	$scope.typeViewPicture = 'all';
-        $scope.getAllPokemons = function(){
-            pokemonsFactory.all().then(function(data){
-                $scope.allPokemons = data;
-            });
-        };
-        $scope.getAllPokemons();
+
+        var type = $routeParams.type;
+
+        if(type){
+            $scope.getTypePokemons = function(type){
+                pokemonsFactory.byType(type).then(function(data){
+                    $scope.allPokemons = data;
+                });
+            };
+            $scope.getTypePokemons(type); 
+        }else {
+            $scope.getAllPokemons = function(){
+                pokemonsFactory.all().then(function(data){
+                    $scope.allPokemons = data;
+                });
+            };
+            $scope.getAllPokemons();
+        }
+
+        
     }]);
 })();
