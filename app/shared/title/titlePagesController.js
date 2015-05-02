2250fed1207c58b19ab274e18f00455b32cc72f8
David Castillo <davecas26@gmail.com>
2015-05-02 18:54:32 -0430

    se agrego la nueva ruta al condicional

--- app/shared/title/titlePagesController.js
+++ app/shared/title/titlePagesController.js
@@ -2,15 +2,17 @@
     angular.module('titlePagesController', [])
 
     .controller('titlePagesController', [ 
-    '$scope', '$location',
-    function ($scope,  $location) {
-    	
+    '$scope', '$location', '$routeParams',
+    function ($scope,  $location, $routeParams) {
+        
     	if($location.path() === '/home'){
             $scope.typeViewTitle = 'home';
         }else if($location.path() === '/pokemons'){
             $scope.typeViewTitle = 'pokemons';
         }else if($location.path() === '/contact'){
             $scope.typeViewTitle = 'contact';
+        }else if($location.path() === '/'+$routeParams.type){
+            $scope.typeViewTitle = 'pokemons';
         }
         
     }]);
