002c5dd15c29be7b320f6499c5b8e4c721cb96d9
David Castillo <davecas26@gmail.com>
2015-05-06 20:26:36 -0430

    se agregaron las direciones de la pagina

--- app/components/pokemons/pokemonsController.js
+++ app/components/pokemons/pokemonsController.js
@@ -1,21 +1,24 @@
 (function(){
-    angular.module('pokemonsController', ['pokemonsService','pokemonNameDirective','pokemonPictureDirective','pokemonTypeDirective'])
+    angular.module('pokemonsController', ['pokemonsService','pokemonNameDirective','pokemonPictureDirective','pokemonTypeDirective','searchDirective'])
 
     .controller('pokemonsController', [ 
-    '$scope', '$http', 'pokemonsFactory', '$routeParams',
-    function ($scope, $http, pokemonsFactory, $routeParams) {
+    '$scope', '$http', 'pokemonsFactory', '$routeParams', '$rootScope',
+    function ($scope, $http, pokemonsFactory, $routeParams, $rootScope) {
     	$scope.typeViewPicture = 'all';
+        $scope.allPokemons = [];
 
-        var type = $routeParams.type;
-
-        if(type){
+        $scope.type = $routeParams.type;
+        
+        if($scope.type){
+            $rootScope.titlepage = 'Pokefile | '+$scope.type;
             $scope.getTypePokemons = function(type){
                 pokemonsFactory.byType(type).then(function(data){
                     $scope.allPokemons = data;
                 });
             };
-            $scope.getTypePokemons(type); 
+            $scope.getTypePokemons($scope.type); 
         }else {
+            $rootScope.titlepage = 'Pokefile | Pokemons';
             $scope.getAllPokemons = function(){
                 pokemonsFactory.all().then(function(data){
                     $scope.allPokemons = data;
