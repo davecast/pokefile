88c5cf694564c50fd34e14badfe8c564bd6f2e78
David Castillo <davecas26@gmail.com>
2015-06-18 11:01:41 -0430

    se creo arrays para datos de links

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -7,6 +7,30 @@
     	
     	$rootScope.titlepage = 'Pokefile | Home';
         
+        $scope.projects=[
+							{
+							    'name': 'Mejorandola-Pokedex',
+							    'url': 'https://github.com/proyectos-mejorandola/pokedex'
+							},
+							{
+							    'name': 'Formatcom- AngularBase',
+							    'url': 'https://github.com/formatCom/baseAngularjs'
+							}
+		           		];
+        $scope.lincenses=[
+							{
+							    'name': 'PokéApi',
+							    'url': 'http://pokeapi.co/'
+							},
+							{
+							    'name': 'Pokémon Database',
+							    'url': 'http://pokemondb.net/'
+							},
+							{
+							    'name': 'veekun/pokedex',
+							    'url': 'https://github.com/veekun/pokedex'
+							}
+		           		];
     }]);
 
 })();
